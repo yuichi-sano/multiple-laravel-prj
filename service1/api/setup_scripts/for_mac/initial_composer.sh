f79c5bafb6b4e192280d7ab7cc98d577846c412b
yuichi.sano <yuichi.sano@insight-inc.biz>
2022-03-03 09:35:22 +0900

    アーキテクト図微修正 proxyサーバ用docker追加、vagrantFile対応

--- service1/api/setup_scripts/for_mac/initial_composer.sh
+++ service1/api/setup_scripts/for_mac/initial_composer.sh
@@ -1,2 +1,2 @@
 #!bin/bash
-docker run --rm --name sample -u "$(id -u):$(id -g)" -v $(pwd):/var/www/html  -w /var/www/html  -it laravelsail/php80-composer:latest composer install --ignore-platform-reqs
+docker run --rm --name sample -u "$(id -u):$(id -g)" -v $(pwd):/var/www/html  -w /var/www/html  -it laravelsail/php80-composer:latest@sha256:748d7e4a6f3696447f6bca391abcae91a73249c57b9e70380e911370a8165c9f composer install --ignore-platform-reqs
